7b0d92f1b1c1baf8fd136ac684e9d787335e27ca
Alex Miller <asmiller1989@gmail.com>
2017-03-09 20:10:03 -0800

    Updates to use 64 byte word size for uint8.

--- contract.py
+++ contract.py
@@ -2,17 +2,26 @@
 import sha3
 
 def getBracketTx(south, west, east, midwest, final_four, championship):
-    header = sha3.keccak_256('setBracket(uint8[15],uint8[15],uint8[15],uint8[15],uint8[4],uint8[2])')
+    header = sha3.keccak_256('setBracket(uint8[14],uint8[14],uint8[14],uint8[14],uint8[4],uint8[2])')
     data = header.hexdigest()[:8]
+    print south
+    print west
+    print east
+    print midwest
     for region in [south, west, east, midwest]:
         for pick in region:
-            data += str(pick).zfill(8)
+            h = hex(int(pick))[2]
+            data += str(h).zfill(64)
+            # data += str(pick).zfill(64)
     for i in range(2):
         for j in range(2):
-            print final_four[i][j]
-            data += str(final_four[i][j]).zfill(8)
-    data += str(championship[0]).zfill(8)
-    data += str(championship[1]).zfill(8)
+            h = hex(int(final_four[i][j]))[2]
+            data += str(h).zfill(64)
+            # data += str(final_four[i][j]).zfill(64)
+    data += str(hex(int(championship[0]))[2]).zfill(64)
+    data += str(hex(int(championship[1]))[2]).zfill(64)
+    # data += str(championship[0]).zfill(64)
+    # data += str(championship[1]).zfill(64)
     return '0x' + data
 
 def getCheckTx(addr):
--- contracts/madness.sol
+++ contracts/madness.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.8;
 
 contract Madness {
-  address owner;
+  address public owner;
   uint COST = 500000000000000000; // 0.5 ETH
   uint STOP_TIME = 1489683600; // Time the tournament starts
   uint FINAL_TIME = 1491530400; // 48 hours after the tournament ends
@@ -12,10 +12,10 @@ contract Madness {
   // Within each bracket, games are defined starting from the round of 64 and going down.
   struct bracket {
     // There are four quarter brackets (East, West, North, South) in a fullBracket
-    uint8[15] south;
-    uint8[15] west;
-    uint8[15] east;
-    uint8[15] midwest;
+    uint8[14] south;
+    uint8[14] west;
+    uint8[14] east;
+    uint8[14] midwest;
 
     // Index is 0=South, 1=West, 2=East, 3=Midwest
     uint8[4] quarterBracketScore;
@@ -56,16 +56,16 @@ contract Madness {
 
   /**
    * Setup a bracket!
-   * param {uint8[15]} south - picks in south quarter
-   * param {uint8[15]} west - picks in west quarter
-   * param {uint8[15]} east - picks in east quarter
-   * param {uint8[15]} midwest - picks in midwest quarter
+   * param {uint8[14]} south - picks in south quarter
+   * param {uint8[14]} west - picks in west quarter
+   * param {uint8[14]} east - picks in east quarter
+   * param {uint8[14]} midwest - picks in midwest quarter
    * param {uint8[4]}  final_four - picks for final Four [[region, seed], ]
    * param {uint8[2]}  championship - picks for champ [region, seed]
 
    */
-  function setBracket(uint8[15] south, uint8[15] west, uint8[15] east,
-  uint8[15] midwest, uint8[4] finalFour, uint8[2] championship) payable
+  function setBracket(uint8[14] south, uint8[14] west, uint8[14] east,
+  uint8[14] midwest, uint8[4] finalFour, uint8[2] championship) payable
   public returns (bool) {
 
     if (msg.sender == owner) {
@@ -80,22 +80,17 @@ contract Madness {
       return true;
     } else {
       if (now > STOP_TIME) { return false; }
-      if (!userBrackets[msg.sender].started) {
-        if (msg.value < COST) { return false; }
-        if (msg.value > COST) { if (!msg.sender.send(COST - msg.value)) { throw; } }
-        pool += COST;
-        userBrackets[msg.sender].started = true;
-        userBrackets[msg.sender].south = south;
-        userBrackets[msg.sender].west = west;
-        userBrackets[msg.sender].east = east;
-        userBrackets[msg.sender].midwest = midwest;
-        userBrackets[msg.sender].finalFour[0] = [finalFour[0], finalFour[1]];
-        userBrackets[msg.sender].finalFour[1] = [finalFour[2], finalFour[3]];
-        userBrackets[msg.sender].championship = championship;
-      } else {
-        // If the user already has a bracket, return all monies.
-        if (!msg.sender.send(msg.value)) { throw; }
-      }
+      if (msg.value < COST) { return false; }
+      if (msg.value > COST) { if (!msg.sender.send(COST - msg.value)) { throw; } }
+      pool += COST;
+      userBrackets[msg.sender].started = true;
+      userBrackets[msg.sender].south = south;
+      userBrackets[msg.sender].west = west;
+      userBrackets[msg.sender].east = east;
+      userBrackets[msg.sender].midwest = midwest;
+      userBrackets[msg.sender].finalFour[0] = [finalFour[0], finalFour[1]];
+      userBrackets[msg.sender].finalFour[1] = [finalFour[2], finalFour[3]];
+      userBrackets[msg.sender].championship = championship;
     }
     return true;
   }
@@ -159,8 +154,38 @@ contract Madness {
   // GETTERS
   //=========================================================
 
+  function getQuarter(address user, uint region) public constant returns (uint8[14]) {
+    if (region == 1) {
+      return userBrackets[user].south;
+    } else if (region == 2) {
+      return userBrackets[user].west;
+    } else if (region == 3) {
+      return userBrackets[user].east;
+    } else if (region == 4) {
+      return userBrackets[user].midwest;
+    }
+  }
+  function getFinals(address user) public constant returns (uint8[6]) {
+    return [userBrackets[user].finalFour[0][0],userBrackets[user].finalFour[0][1],userBrackets[user].finalFour[1][0],userBrackets[user].finalFour[1][1],userBrackets[user].championship[0],userBrackets[user].championship[1]];
+  }
+  function getOracleQuarter(uint region) public constant returns (uint8[14]) {
+    if (region == 1) {
+      return oracleBracket.south;
+    } else if (region == 2) {
+      return oracleBracket.west;
+    } else if (region == 3) {
+      return oracleBracket.east;
+    } else if (region == 4) {
+      return oracleBracket.midwest;
+    }
+  }
+  function getOracleFinals() public constant returns (uint8[6]) {
+    return [oracleBracket.finalFour[0][0],oracleBracket.finalFour[0][1],oracleBracket.finalFour[1][0],oracleBracket.finalFour[1][1],oracleBracket.championship[0],oracleBracket.championship[1]];
+  }
+
   function getCurrentScore(address user) public constant returns (uint8) {
     uint8 score = getQuarterScore(user);
+    if (userBrackets[user].started) { score += 1; }
     score += getFinalScores(user);
     return score;
   }
@@ -168,10 +193,10 @@ contract Madness {
   function getQuarterScore(address user) public constant returns (uint8) {
     uint8 score = 0;
     for (uint i=0; i<15; i++) {
-      if (userBrackets[user].south[i] == oracleBracket.south[i]) { score += 1; }
-      if (userBrackets[user].west[i] == oracleBracket.west[i]) { score += 1; }
-      if (userBrackets[user].east[i] == oracleBracket.east[i]) { score += 1; }
-      if (userBrackets[user].midwest[i] == oracleBracket.midwest[i]) { score += 1; }
+      if (oracleBracket.south[i] > 0 && userBrackets[user].south[i] == oracleBracket.south[i]) { score += 1; }
+      if (oracleBracket.west[i] > 0 && userBrackets[user].west[i] == oracleBracket.west[i]) { score += 1; }
+      if (oracleBracket.east[i] > 0 && userBrackets[user].east[i] == oracleBracket.east[i]) { score += 1; }
+      if (oracleBracket.midwest[i] > 0 && userBrackets[user].midwest[i] == oracleBracket.midwest[i]) { score += 1; }
     }
     return score;
   }
@@ -180,14 +205,20 @@ contract Madness {
     uint8 score = 0;
     for (uint i=0; i<2; i++) {
       // Add final four scores (worth 2 points for correct prediction)
-      if (userBrackets[user].finalFour[i][0] == oracleBracket.finalFour[i][0]
-        && userBrackets[user].finalFour[i][1] == oracleBracket.finalFour[i][1]) {
+      if (oracleBracket.finalFour[0][i] > 0
+        && userBrackets[user].finalFour[i][0] == oracleBracket.finalFour[i][0]
+        && userBrackets[user].finalFour[i][1] == oracleBracket.finalFour[i][1]
+      ) {
         score += 2;
       }
     }
     // Championship prediction is worth 4 points
-    if (userBrackets[user].championship[0] == oracleBracket.championship[0]
-      && userBrackets[user].championship[1] == oracleBracket.championship[1]) {
+    if (
+      userBrackets[user].championship[0] == oracleBracket.championship[0]
+      && oracleBracket.championship[0] > 0
+      && userBrackets[user].championship[1] == oracleBracket.championship[1]
+      && oracleBracket.championship[1] > 0
+    ) {
       score += 4;
     }
     return score;
